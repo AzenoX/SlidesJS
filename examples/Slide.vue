82a5b5404a63bea83842f0f9166efc9082a311f3
Alexis <azen0x.alexis@gmail.com>
2021-02-02 20:02:36 +0100

    Added Vue Component Example

---
+++ examples/Slide.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="div" @click="slideUp">
+    {{ lorem }}
+  </div>
+</template>
+
+<script>
+import {defineComponent} from "vue";
+import SlidesJS from "@azenox/slidesjs/SlidesJS.js";
+
+export default defineComponent({
+  name: "Slide",
+  data() {
+    return {
+      lorem: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae, deleniti facilis fugit harum, laboriosam maiores modi perferendis quod reprehenderit soluta suscipit unde! Aliquid amet minima nihil, perferendis provident repellat."
+    }
+  },
+  methods: {
+    slideUp(e) {
+      const el = e.target;
+      new SlidesJS(el).slideToggleY();
+    }
+  }
+})
+</script>
+
+<style scoped>
+#div{
+  background: orange;
+  margin: 2em;
+  padding: 2em;
+}
+</style>
